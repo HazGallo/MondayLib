6f071e4197d2357085591c106f53d976b0b13e45
HazGallo <hazleyjarquin03@gmail.com>
2024-01-08 16:37:35 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Button from "monday-ui-react-core/dist/Button";
+
+function App() {
+  return (
+    <>
+      <Button>Hola</Button>
+    </>
+  );
+}
+
+export default App;
